fb10e66c86289c48f364c37791769381afdd5a5f
Tupay Felkin <tupai@live.co.uk>
2025-06-10 11:36:51 +0100

    FEAT: Create class HashMap and base methods

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,21 +1,59 @@
-class HashMap {
-    constructor(capacity= 16, loadFactor = .75){
+export class HashMap {
+    constructor(capacity = 16, loadFactor = .75){
         this.table = new Array(capacity)
         this.capacity = capacity;
         this.loadFactor = loadFactor;
     }
-}
 
-function stringToNumber(string) {
-    let hashCode = 0;
-    for (let i = 0; i < string.length; i++) {
-        hashCode += string.charCodeAt(i);
+    hash(key){
+        let hashValue = 0;
+        for (let i = 0; i < key.length; i++) {
+            hashValue = (hashValue + key.charCodeAt(i) * i)% this.capacity;
+        }
+        return hashValue;
     }
 
-    return hashCode;
+    set(key,value) {
+        const index = this.hash(key);
+        this.table[index] = value;
+    }
+
+    remove(key){
+        const index = this.hash(key);
+        delete this.table[index];
+    }
+
+    get(key){
+        const index = this.hash(key);
+        return this.table[index];
+    }
+
+    has(key) {
+        const index = this.hash(key)
+        return (this.table[index] != null)
+    }
+
+    length() {
+        return this.table.length;
+    }
+
+    clear(){
+        delete this.table;
+        this.table = new Array(this.capacity)
+    }
+
 }
 
-function hash(name, surname) {
-    return stringToNumber(name)+ stringToNumber(surname);
+// export function stringToNumber(string) {
+//     let hashCode = 0;
+//     for (let i = 0; i < string.length; i++) {
+//         hashCode += string.charCodeAt(i);
+//     }
 
-}
+//     return hashCode;
+// }
+
+// export function hash(name, surname) {
+//     return stringToNumber(name) + stringToNumber(surname);
+
+// }   
